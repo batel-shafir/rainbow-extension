61d4c9f928055c21cb5936ff78b6bc9396c6f282
batel-shafir <batyushafir+1@gmail.com>
2020-06-11 20:46:26 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var body = document.body;
+
+replaceText(body);
+
+
+function replaceText(element){
+  if(element.hasChildNodes()){
+    element.childNodes.forEach(replaceText);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    if(element.textContent.match(/coronavirus/gi)) {
+        const newElement = document.createElement('span');
+        newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, 
+            '<span id="rainbow" class="rainbow">$1</span>')
+            element.replaceWith(newElement);
+    }
+  }
+   
+}
+
